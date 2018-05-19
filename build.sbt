3c2f5676b3fc2b74e8ba8c58193eb74bb8d6cc67
Evgeny Veretennikov <vere10@yandex-team.ru>
2018-05-19 16:01:12 +0300

    generate Repr

--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ scalacOptions += "-Xlog-implicits"
 scalacOptions += "-Xprint:typer"
 
 libraryDependencies ++= Seq(
+  "org.scala-lang" % "scala-compiler" % scalaVersion.toString,
   "org.scala-lang" % "scala-reflect" % scalaVersion.toString,
   "com.chuusai" %% "shapeless" % "2.3.3",
   "com.google.protobuf" % "protobuf-java" % "3.5.1",
--- src/main/scala/com/github/evis/shapeless/protobuf/ShapelessProtobufMacros.scala
+++ src/main/scala/com/github/evis/shapeless/protobuf/ShapelessProtobufMacros.scala
@@ -1,6 +1,7 @@
 package com.github.evis.shapeless.protobuf
 
 import proto.test.LittleFile.MyMessage
+import shapeless.{::, HNil}
 
 import scala.reflect.macros.whitebox
 
@@ -15,11 +16,70 @@ class ShapelessProtobufMacros(val c: whitebox.Context) {
     val clsName = TypeName(c.freshName("anon$"))
     q"""
       final class $clsName extends _root_.shapeless.Generic[$tpe] {
-        type Repr = _root_.shapeless.::[String, _root_.shapeless.::[Int, _root_.shapeless.::[Boolean, _root_.shapeless.HNil]]]
+        type Repr = ${reprTypTree(tpe)}
         def to(p: $tpe): Repr = _root_.shapeless.::(p.getMyString, _root_.shapeless.::(p.getMyInt, _root_.shapeless.::(p.getMyBool, _root_.shapeless.HNil)))
         def from(p: Repr): $tpe = p match { case _root_.shapeless.::(s, _root_.shapeless.::(i, _root_.shapeless.::(b, _root_.shapeless.HNil))) => _root_.proto.test.LittleFile.MyMessage.newBuilder().setMyString(s).setMyInt(i).setMyBool(b).build() }
       }
-      new $clsName(): _root_.shapeless.Generic.Aux[$tpe, _root_.shapeless.::[String, _root_.shapeless.::[Int, _root_.shapeless.::[Boolean, _root_.shapeless.HNil]]]]
+      new $clsName(): _root_.shapeless.Generic.Aux[$tpe, ${reprTypTree(tpe)}]
     """
+  }
+
+  def hnilTpe = typeOf[HNil]
+  def hconsTpe = typeOf[::[_, _]].typeConstructor
+
+  def reprTypTree(tpe: Type): Tree = {
+    val fields = fieldsOf(tpe)
+    mkCompoundTypTree(hnilTpe, hconsTpe, fields)
+  }
+
+  def fieldsOf(tpe: Type): List[Type] = {
+    val tpeInterface = tpe.baseClasses.filter {
+      _.fullName match {
+        case "com.google.protobuf.MessageOrBuilder" => false
+        case name => name.endsWith("OrBuilder")
+      }
+    }.head.asType.toType // TODO head may throw exception
+    tpeInterface.decls.sorted.collect {
+      case sym: TermSymbol =>
+        sym.typeSignatureIn(tpe).finalResultType
+    }.filter {
+      _.typeSymbol.fullName match {
+        case "com.google.protobuf.ByteString" => false
+        case _ => true
+      }
+    }
+  }
+
+  def mkAttributedRef(tpe: Type): Tree = {
+    // TODO review this! just copy-pasted from shapeless
+    val global = c.universe.asInstanceOf[scala.tools.nsc.Global]
+    val gTpe = tpe.asInstanceOf[global.Type]
+    val pre = gTpe.prefix
+    val sym = gTpe.typeSymbol
+    global.gen.mkAttributedRef(pre, sym).asInstanceOf[Tree]
+  }
+
+  def mkAttributedRef(pre: Type, sym: Symbol): Tree = {
+    // TODO review this! just copy-pasted from shapeless
+    val global = c.universe.asInstanceOf[scala.tools.nsc.Global]
+    val gPre = pre.asInstanceOf[global.Type]
+    val gSym = sym.asInstanceOf[global.Symbol]
+    global.gen.mkAttributedRef(gPre, gSym).asInstanceOf[Tree]
+  }
+
+  def mkCompoundTypTree(nil: Type, cons: Type, items: List[Type]): Tree = {
+    // TODO review this! just copy-pasted from shapeless
+    items.foldRight(mkAttributedRef(nil): Tree) { case (tpe, acc) =>
+      AppliedTypeTree(mkAttributedRef(cons), List(mkTypTree(tpe), acc))
+    }
+  }
+
+  def mkTypTree(tpe: Type): Tree = {
+    // TODO review this! just copy-pasted from shapeless with a bit of change
+    tpe match {
+      case SingleType(pre @ SingleType(_, _), sym) =>
+        SingletonTypeTree(mkAttributedRef(pre, sym))
+      case t => tq"$t"
+    }
   }
 }
