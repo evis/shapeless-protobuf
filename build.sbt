cd30469405e947e7096710ac8ca51305a090b39d
Evgeny Veretennikov <vere10@yandex-team.ru>
2018-05-19 11:20:47 +0300

    add scalacheck example

--- build.sbt
+++ build.sbt
@@ -11,6 +11,7 @@ libraryDependencies ++= Seq(
   "org.scala-lang" % "scala-reflect" % scalaVersion.toString,
   "com.chuusai" %% "shapeless" % "2.3.3",
   "com.google.protobuf" % "protobuf-java" % "3.5.1",
+  "com.github.alexarchambault" %% "scalacheck-shapeless_1.13" % "1.1.6",
 )
 
 addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
--- src/main/scala/com/github/evis/shapeless/protobuf/example/Main.scala
+++ src/main/scala/com/github/evis/shapeless/protobuf/example/Main.scala
@@ -1,12 +1,16 @@
 package com.github.evis.shapeless.protobuf.example
 
 import proto.test.LittleFile.MyMessage
-import shapeless.{Generic, HNil, ::}
+import shapeless.{::, Generic, HNil}
 import com.github.evis.shapeless.protobuf._
+import org.scalacheck.Arbitrary
+import org.scalacheck.ScalacheckShapeless._
 
 object Main extends App {
 
   println {
     implicitly[Generic.Aux[MyMessage, String :: Int :: Boolean :: HNil]].from("hello" :: 1234 :: true :: HNil)
   }
+
+  println(implicitly[Arbitrary[MyMessage]].arbitrary.sample.get)
 }
