46e51f0f7db7b24d3106d6f0dd1f6c76670b80c9
Evgeny Veretennikov <vere10@yandex-team.ru>
2018-05-20 10:59:03 +0300

    fix test for new model

--- src/main/scala/com/github/evis/shapeless/protobuf/example/Main.scala
+++ src/main/scala/com/github/evis/shapeless/protobuf/example/Main.scala
@@ -3,17 +3,19 @@ package com.github.evis.shapeless.protobuf.example
 import proto.test.LittleFile.MyMessage
 import shapeless.{::, Generic, HNil}
 import com.github.evis.shapeless.protobuf._
+import com.google.protobuf.ByteString
 import org.scalacheck.Arbitrary
 import org.scalacheck.ScalacheckShapeless._
+import proto.test.LittleFile.MyMessage.MyEnum
 
 object Main extends App {
 
   println {
-    implicitly[Generic.Aux[MyMessage, String :: Int :: Boolean :: HNil]].from("hello" :: 1234 :: true :: HNil)
+    implicitly[Generic.Aux[MyMessage, String :: Int :: Boolean :: MyEnum :: ByteString :: HNil]].from("hello" :: 1234 :: true :: MyEnum.HER_VALUE :: ByteString.copyFrom("hello bytes", "UTF-8") :: HNil)
   }
 
   println {
-    implicitly[Generic.Aux[MyMessage, String :: Int :: Boolean :: HNil]].to(MyMessage.newBuilder().setMyString("hello").setMyInt(1234).setMyBool(true).build())
+    implicitly[Generic.Aux[MyMessage, String :: Int :: Boolean :: HNil]].to(MyMessage.newBuilder().setMyString("hello").setMyInt(1234).setMyBool(true).setMyEnum(MyEnum.HER_VALUE).setMyBytes(ByteString.copyFrom("hello bytes", "UTF-8")).build())
   }
 
   println(implicitly[Arbitrary[MyMessage]].arbitrary.sample.get)
