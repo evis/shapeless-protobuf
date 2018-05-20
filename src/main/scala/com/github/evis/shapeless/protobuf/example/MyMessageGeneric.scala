91952f0cc918ea295cdeedfdf8ef900ff8dd8266
Evgeny Veretennikov <vere10@yandex-team.ru>
2018-05-20 23:00:36 +0300

    update example

--- src/main/scala/com/github/evis/shapeless/protobuf/example/MyMessageGeneric.scala
+++ src/main/scala/com/github/evis/shapeless/protobuf/example/MyMessageGeneric.scala
@@ -1,20 +1,46 @@
 package com.github.evis.shapeless.protobuf.example
 
+import com.google.protobuf.ByteString
 import proto.test.LittleFile.MyMessage
+import proto.test.LittleFile.MyMessage.{Inner, MyEnum}
 import shapeless.{::, Generic, HNil}
 
 object MyMessageGeneric {
 
-  implicit val myMessageGeneric: Generic.Aux[MyMessage, String :: Int :: Boolean :: HNil] =
+  implicit val myMessageGeneric: Generic.Aux[MyMessage, String :: Int :: Boolean :: MyEnum :: ByteString :: Int :: ByteString :: (Long :: String :: Long :: HNil) :: HNil] =
     new Generic[MyMessage] {
 
-      override type Repr = String :: Int :: Boolean :: HNil
+      override type Repr = String :: Int :: Boolean :: MyEnum :: ByteString :: Int :: ByteString :: (Long :: String :: Long :: HNil) :: HNil
 
-      override def to(t: MyMessage): Repr = t.getMyString :: t.getMyInt :: t.getMyBool :: HNil
+      override def to(t: MyMessage): Repr = {
+        t.getMyString ::
+        t.getMyInt ::
+        t.getMyBool ::
+        t.getMyEnum ::
+        t.getMyBytes ::
+        t.getMyValue ::
+        t.getMyIntBytes ::
+          (t.getInner.getLongValue :: t.getInner.getKeyValue :: t.getInner.getSomethingOrBuilder :: HNil) ::
+        HNil
+      }
 
       override def from(r: Repr): MyMessage = r match {
-        case s :: i :: b :: HNil =>
-          MyMessage.newBuilder().setMyString(s).setMyInt(i).setMyBool(b).build()
+        case myString :: myInt :: myBool :: myEnum :: myBytes :: myValue :: myIntBytes ::
+          (longValue :: keyValue :: somethingOrBuilder :: HNil) :: HNil =>
+          MyMessage.newBuilder()
+            .setMyString(myString)
+            .setMyInt(myInt)
+            .setMyBool(myBool)
+            .setMyEnum(myEnum)
+            .setMyBytes(myBytes)
+            .setMyValue(myValue)
+            .setMyIntBytes(myIntBytes)
+            .setInner(
+              Inner.newBuilder()
+                .setLongValue(longValue)
+                .setKeyValue(keyValue)
+                .setSomethingOrBuilder(somethingOrBuilder)
+            ).build()
       }
     }
 
