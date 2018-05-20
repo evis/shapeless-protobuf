0ebb146568620a86f5c0c5caa2875ec2b509504b
Evgeny Veretennikov <vere10@yandex-team.ru>
2018-05-20 09:22:09 +0300

    rm macros from API

--- src/main/scala/com/github/evis/shapeless/protobuf/ShapelessProtobufMacros.scala
+++ src/main/scala/com/github/evis/shapeless/protobuf/ShapelessProtobufMacros.scala
@@ -4,7 +4,7 @@ import shapeless.{::, HNil}
 
 import scala.reflect.macros.whitebox
 
-class ShapelessProtobufMacros(val c: whitebox.Context) {
+private[protobuf] class ShapelessProtobufMacros(val c: whitebox.Context) {
   import c.universe._
 
   def protobufProduct[T: WeakTypeTag, R: WeakTypeTag]: Tree = {
