4e826c8425bf0535c43167bbedffbd60fd23fff3
Evgeny Veretennikov <vere10@yandex-team.ru>
2018-05-22 21:57:39 +0300

    better tests

--- src/test/scala/com/github/evis/shapeless/protobuf/ProtobufGenericSpec.scala
+++ src/test/scala/com/github/evis/shapeless/protobuf/ProtobufGenericSpec.scala
@@ -1,23 +1,33 @@
 package com.github.evis.shapeless.protobuf
 
-import org.scalacheck.Prop.{forAll, BooleanOperators}
+import com.github.evis.shapeless.protobuf.TestMessages._
+import org.scalacheck.Prop.{BooleanOperators, forAll}
 import org.scalacheck.Properties
 import org.scalacheck.ScalacheckShapeless._
-import com.github.evis.shapeless.protobuf.TestMessages._
-import shapeless.{::, Generic, HNil}
+import shapeless.ops.product.ToHList
+import shapeless.{::, Generic, HList, HNil}
 
 object ProtobufGenericSpec extends Properties("ProtobufGeneric") {
 
+  implicit class HListToOps[R <: HList](val hlist: R) extends AnyVal {
+
+    def to[T](implicit gen: Generic.Aux[T, R]): T = gen.from(hlist)
+  }
+
+  implicit class ToHListOps[T](val from: T) extends AnyVal {
+
+    def toHList(implicit toHList: ToHList[T]): toHList.Out = toHList(from)
+  }
+
   property("convert primitives to generic") = {
     forAll { p: Primitives =>
-      val repr = implicitly[Generic.Aux[Primitives, String :: Int :: Long :: Boolean :: HNil]].to(p)
-      repr == p.getTestString :: p.getTestInt :: p.getTestLong :: p.getTestBool :: HNil
+      p.toHList == p.getTestString :: p.getTestInt :: p.getTestLong :: p.getTestBool :: HNil
     }
   }
 
   property("convert generic to primitives") = {
     forAll { repr: (String :: Int :: Long :: Boolean :: HNil) =>
-      val p = implicitly[Generic.Aux[Primitives, String :: Int :: Long :: Boolean :: HNil]].from(repr)
+      val p = repr.to[Primitives]
       repr match {
         case a :: b :: c :: d :: HNil =>
           p == Primitives.newBuilder().setTestString(a).setTestInt(b).setTestLong(c).setTestBool(d).build()
@@ -30,8 +40,18 @@ object ProtobufGenericSpec extends Properties("ProtobufGeneric") {
       // TODO filter unrecognized automatically for protobuf enums
       e != TestEnum.UNRECOGNIZED ==> {
         val p = WithEnum.newBuilder().setTestString(s).setTestEnum(e).build()
-        val repr = implicitly[Generic.Aux[WithEnum, String :: TestEnum :: HNil]].to(p)
-        repr == p.getTestString :: p.getTestEnum :: HNil
+        p.toHList == p.getTestString :: p.getTestEnum :: HNil
+      }
+    }
+  }
+
+  property("convert generic to with enum") = {
+    forAll { (s: String, e: TestEnum) =>
+      // TODO filter unrecognized automatically for protobuf enums
+      e != TestEnum.UNRECOGNIZED ==> {
+        val repr = s :: e :: HNil
+        val p = repr.to[WithEnum]
+        p == WithEnum.newBuilder().setTestString(s).setTestEnum(e).build()
       }
     }
   }
